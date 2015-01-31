fc00f7dbffef7b49b7cb7b1e0415c2f7f05d1be5
Aimee Morgan <aimeelmorgan@gmail.com>
2015-01-30 17:09:02 -0800

    BCK-3639 Adding description and owner name fields to jobs
    This commit adds backend support for description and job owner
    name fields, plus support for viewing and editing these fields
    via the Chronos web UI.

--- src/main/resources/assets/app/scripts/models/base_job.js
+++ src/main/resources/assets/app/scripts/models/base_job.js
@@ -26,8 +26,8 @@ function($, Backbone, _, moment, BaseJobValidations) {
   }
 
   BaseWhiteList = [
-    'name', 'command', 'owner', 'async', 'epsilon', 'executor', 'disabled',
-    'cpus', 'mem', 'disk', 'highPriority'
+    'name', 'command', 'description', 'owner', 'ownerName', 'async', 'epsilon', 'executor', 
+    'disabled', 'cpus', 'mem', 'disk', 'highPriority'
   ];
 
   BaseJobModel = Backbone.Model.extend({
